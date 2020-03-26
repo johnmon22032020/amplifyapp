ff09aadfd457e2d9bd58ea206b82d07d1f2f264d
johnmon22032020 <62694811+johnmon22032020@users.noreply.github.com>
2020-03-26 11:22:50 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          //Edit <code>src/App.js</code> and save to reload Part 2.
           John-mon ReactApp integration with AWS Amplify and GitHub
         </p>
         <a
